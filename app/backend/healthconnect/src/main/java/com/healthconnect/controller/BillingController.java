31f32acccc8665800f7d2357d0fcd62e8fc84a8b
Nathaniel Gillam <ngillam@ewu.edu>
2024-08-07 20:40:37 -0700

    Payment stuff working
    I lied the payment screen shows accepted payment now but doesn't update the databasse

--- app/backend/healthconnect/src/main/java/com/healthconnect/controller/BillingController.java
+++ app/backend/healthconnect/src/main/java/com/healthconnect/controller/BillingController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("http://localhost:63342")
 @RestController
 @RequestMapping("/billing")
 public class BillingController {
